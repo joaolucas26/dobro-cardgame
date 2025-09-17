e0df03fea4620ad0770766ba9755d721406e0378
Joao Lucas <joao.lucas26@outlook.com>
2025-09-16 21:43:46 -0300

    adiciona acao compra e passa turno manual

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ import random
 from game.player import Player
 from game.game import Game
 from game.card import Joker
-from game.rules import MIN_PLAYERS, MAX_PLAYERS
+from game.rules import MIN_PLAYERS, MAX_PLAYERS, NOT_YOUR_TURN_EVENT_LIST
 from utils.payloads import (
     send_update_room,
     send_update_game_status,
@@ -21,15 +21,24 @@ async def handler(websocket):
     connected_clients = [x for x in GLOBAL["clients"] if x["websocket"] != None]
     disconnected_clients = [x for x in GLOBAL["clients"] if x["websocket"] == None]
 
+    if len(connected_clients) >= MAX_PLAYERS:
+        response = {
+            "type": "error",
+            "message": "Jogo já esta cheio! Tente novamente mais tarde.",
+        }
+        await websocket.send(json.dumps(response))
+        await websocket.close()
+        return
+
     if GLOBAL["game"]:
         if disconnected_clients:
             disconnected_clients[0]["websocket"] = websocket
             player = disconnected_clients[0]["player"]
-            disconnected_clients_ = [
+            all_player_reconnected = [
                 x for x in GLOBAL["clients"] if x["websocket"] == None
-            ]
+            ] == []
 
-            if not disconnected_clients_:
+            if all_player_reconnected:
                 GLOBAL["game"].is_paused = False
 
             await send_update_game_status()
@@ -37,7 +46,7 @@ async def handler(websocket):
         else:
             response = {
                 "type": "error",
-                "message": "Game Already Started. Try again later.",
+                "message": "Jogo já iniciou! Tente novamente mais tarde!",
             }
             await websocket.send(json.dumps(response))
             await websocket.close()
@@ -50,20 +59,15 @@ async def handler(websocket):
             client["player"].is_ready = False
         await send_update_room()
 
-    if len(connected_clients) >= MAX_PLAYERS:
-        response = {"type": "error", "message": "Game is Full. Try again later."}
-        await websocket.send(json.dumps(response))
-        await websocket.close()
-        return
-
     try:
         async for message in websocket:
             try:
                 message = json.loads(message)
                 if message["type"] == "PLAYER_READY":
                     if GLOBAL["game"]:
-                        raise Exception("Game Already Started!")
-                    if not player.is_ready:
+                        raise Exception("Jogo já iniciou!")
+
+                    elif not player.is_ready:
                         player.is_ready = True
                         if len(GLOBAL["clients"]) >= MIN_PLAYERS:
                             if all(c["player"].is_ready for c in GLOBAL["clients"]):
@@ -74,18 +78,24 @@ async def handler(websocket):
                     await send_update_room()
 
                 elif GLOBAL["game"] and GLOBAL["game"].is_paused:
+                    raise Exception("Jogo pausado. Espere por uma nova conexão")
+
+                elif not GLOBAL["game"] and message["type"] != "PLAYER_READY":
                     raise Exception(
-                        "Game Paused. Wait till restart or another connection"
+                        "Você precisa estar pronto antes de começar a jogar!"
                     )
 
                 elif GLOBAL["game"] and player != GLOBAL["game"].current_player:
-                    raise Exception("Not your turn")
+                    if message["type"] in NOT_YOUR_TURN_EVENT_LIST:
+                        raise Exception("Não é seu turno!")
 
                 if message["type"] == "PLAY_CARD":
                     list_card_index = message["card_index"]
                     cards_played = [player.hand[index] for index in list_card_index]
                     if len(cards_played) > 2:
-                        raise Exception("Cannot play more than 2 cards")
+                        raise Exception(
+                            "Não é possivel jogar mais do que 2 cartas simultâneas!"
+                        )
 
                     card_value = message.get("card_value")
                     for card in cards_played:
@@ -101,16 +111,32 @@ async def handler(websocket):
                         await send_update_room()
                     await send_update_game_status()
 
+                if message["type"] == "DRAW_CARD":
+                    GLOBAL["game"].draw_card(player)
+                    await send_update_game_status()
+
                 if message["type"] == "END_TURN":
                     GLOBAL["game"].end_turn()
                     await send_update_game_status()
 
                 if message["type"] == "PAUSE_GAME":
+                    if disconnected_clients:
+                        raise Exception(
+                            "Não é possivel despausar/despausa o jogo enquanto existe jogadores faltando! Reinicie o jogo ou aguarde reconexão"
+                        )
                     GLOBAL["game"].is_paused = not GLOBAL["game"].is_paused
                     await send_update_game_status()
 
+                if message["type"] == "PUNISH":
+                    player_index = message["player_index"]
+                    target_player = GLOBAL["clients"][player_index]["player"]
+                    GLOBAL["game"].punish_player(target_player)
+                    await send_update_game_status()
+
             except Exception as e:
+                # print(e)
                 await send_error_message(str(e), player)
+                # raise e
 
     except websockets.exceptions.ConnectionClosedOK:
         print(f"Client {websocket.remote_address} disconnected gracefully.")
@@ -121,7 +147,7 @@ async def handler(websocket):
             x for x in GLOBAL["clients"] if x["websocket"] == websocket
         ]
         if GLOBAL["game"]:
-            if len(disconnected_clients) == len(GLOBAL["clients"] - 1):
+            if len(disconnected_clients) == len(GLOBAL["clients"]) - 1:
                 GLOBAL["game"] = None
                 GLOBAL["clients"] = []
             else:
